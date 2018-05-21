26ebcbe39ccb779a24d10b45e44c8ebf2cfb3a28
Olamilekan Wahab <olamyy53@gmail.com>
2018-05-21 04:36:35 +0100

    Pypa Update

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,9 +7,3 @@ from arxivcli import cli
 def runner():
     return CliRunner()
 
-
-def test_cli(runner):
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert not result.exception
-    assert result.output.strip() == 'Hello, world.'
